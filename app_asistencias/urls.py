411284ed6b4ee302e4dcd207d50463554a839108
Christopher Serrano <tyson.272002@gmail.com>
2024-09-21 19:34:30 -0500

    cambio de endpoint para las asistencias

--- app_asistencias/urls.py
+++ app_asistencias/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import AsistenciaViewSet
 
 router = DefaultRouter()
-router.register(r'asistencias', AsistenciaViewSet)
+router.register(r'asistenciasJSON', AsistenciaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
