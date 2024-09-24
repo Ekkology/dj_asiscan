12c7e55952323fb73c8e6519b6e38550ec89689e
Jesus <normales01@gmail.com>
2024-09-23 20:55:01 -0500

    Prueba 19 configuración de webhook

--- dj_asiscan/view.py
+++ dj_asiscan/view.py
@@ -24,38 +24,20 @@ GITHUB_SECRET = env('secreto_hook') # Cambia esto a tu secreto real
 
 @csrf_exempt  # Exime la verificación CSRF para esta vista
 def github_webhook(request):
-    if request.method == 'POST':
-        # Lee el cuerpo de la solicitud
-        payload = request.body
-        
-        # Verifica la firma
-        signature = request.META.get('HTTP_X_HUB_SIGNATURE')
-        if signature is None:
-            logging.error("No signature provided")
-            return JsonResponse({'status': 'unauthorized'}, status=401)
-        
-        # Calcula la firma HMAC
-        hash_object = hmac.new(GITHUB_SECRET, payload, hashlib.sha1)
-        calculated_signature = 'sha1=' + hash_object.hexdigest()
 
-        # Compara la firma calculada con la firma enviada
-        if not hmac.compare_digest(calculated_signature, signature):
-            logging.error("Invalid signature")
-            return JsonResponse({'status': 'unauthorized'}, status=401)
-
-        # Log para verificar que recibimos el webhook
-        payload_data = json.loads(payload)
-        logging.info("Webhook received: %s", payload_data)
+    if request.method == 'GET':  # Cambia a 'GET' para ejecutar al acceder a la URL
+        # Log para verificar que la vista fue accedida
+        logging.info("Accessed run_script view")
 
         # Ejecuta el script de PowerShell
         try:
-            subprocess.call(["powershell.exe", "-ExecutionPolicy", "Bypass", "-File", "C:\Apache24\htdocs\dj_asiscan\.git\hooks\post-receive.ps1"])
+            subprocess.call(["powershell.exe", "-ExecutionPolicy", "Bypass", "-File", "C:\\Apache24\\htdocs\\dj_asiscan\\.git\\hooks\\post-receive.ps1"])
             return JsonResponse({'status': 'success'})
         except Exception as e:
             logging.error("Error executing script: %s", e)
             return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
     
-    return JsonResponse({'status': 'invalid method'}, status=405)
+    return JsonResponse({'status': 'invalid method'}, status=405)  
 
 
 
